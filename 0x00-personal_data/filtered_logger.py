8fc6c5d36e0a11a40a4b6ae45e6bbec8422910a1
oumaima <oumaimanaanaapro@gmail.com>
2024-07-07 15:56:45 +0100

    1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,7 @@
 """ This script defines multiple functions that deals with personal data """
 
 import logging
-from typing import List
+from typing import List, Optional
 import re
 
 
@@ -21,3 +21,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         pattern = rf'{re.escape(field)}=.*?(?={re.escape(separator)}|$)'
         message = re.sub(pattern, rf'{field}={redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super(RedactingFormatter, self).format(record)
+        message = filter_datum(self.fields, self.REDACTION, message,
+                               self.SEPARATOR)
+
+        return message
