d89202a5f77d1e58c8dd8402cc0787da0767438c
oumaima <oumaimanaanaapro@gmail.com>
2024-07-09 19:43:08 +0100

    4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,11 @@ class Auth:
     """ Auth class definition to manage API auth"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns False for now"""
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        if path in excluded_paths or path + "/" in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ returns None for now"""
