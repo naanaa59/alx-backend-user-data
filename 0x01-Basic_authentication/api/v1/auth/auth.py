7e3e09f626129f1273be86edaa4ca30125cb5c60
oumaima <oumaimanaanaapro@gmail.com>
2024-07-09 20:17:16 +0100

    5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,7 @@ from typing import List, TypeVar
 class Auth:
     """ Auth class definition to manage API auth"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ returns False for now"""
+        """Define which routes don't need authentication"""
         if path is None or excluded_paths is None or not excluded_paths:
             return True
         if path in excluded_paths or path + "/" in excluded_paths:
@@ -16,8 +16,13 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ returns None for now"""
-        return None
+        """ validate all requests to secure the API:"""
+        if request is None:
+            return None
+        auth_header = request.headers.get('Authorization')
+        if auth_header is None:
+            return None
+        return auth_header
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ return None for now"""
