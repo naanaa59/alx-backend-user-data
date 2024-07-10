d287242b8a2b2f4673520798538ceda84df02865
oumaima <oumaimanaanaapro@gmail.com>
2024-07-10 19:57:18 +0100

    6 3

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,13 +16,13 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 auth = getenv("AUTH_TYPE")
-if auth:
-    if auth == "basic_auth":
-        from api.v1.auth.basic_auth import BasicAuth
-        auth = BasicAuth()
-    else:
-        from api.v1.auth.auth import Auth
-        auth = Auth()
+# if auth:
+if auth == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.before_request
