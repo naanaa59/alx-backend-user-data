959d17ea2c294ca7a5a7b8639884b244bbcc4f2c
oumaima <oumaimanaanaapro@gmail.com>
2024-07-11 18:57:48 +0100

    2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,18 @@
 """ Script for SessionAuth class definition that inherits drom Auth class"""
 
 from api.v1.auth.auth import Auth
+from typing import Dict
+import uuid
 
 
 class SessionAuth(Auth):
     """ SessionAuth class definition"""
+    user_id_by_session_id: Dict[str, str] = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
