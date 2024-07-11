81641a538051866e1fa9f3a193285c1e62264e58
oumaima <oumaimanaanaapro@gmail.com>
2024-07-11 18:34:47 +0100

    1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Script for SessionAuth class definition that inherits drom Auth class"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class definition"""
