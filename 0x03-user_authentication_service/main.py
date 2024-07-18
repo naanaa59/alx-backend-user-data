11a7f2317753ca8f6c15a64bc5ddc5c8fd0d62bb
oumaima <oumaimanaanaapro@gmail.com>
2024-07-18 19:59:45 +0100

    20

---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import requests
+
+
+def register_user(email: str, password: str) -> None:
+    """ reigster user method"""
+    form_data = {
+        "email": email,
+        "password": password
+    }
+    response = requests.post("http://localhost:5000/users", data=form_data)
+    data = response.json()
+    if response.status_code == 400:
+        assert data["message"] == "email already registered"
+    else:
+        assert data["email"] == email
+        assert data["message"] == "user created"
+    return None
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """ login user method with wrong password"""
+    form_data = {
+        "email": email,
+        "password": password
+    }
+    response = requests.post("http://localhost:5000/sessions", data=form_data)
+    assert response.status_code == 401
+
+
+def log_in(email: str, password: str) -> str:
+    """ login user method with right password"""
+    form_data = {
+        "email": email,
+        "password": password
+    }
+    response = requests.post("http://localhost:5000/sessions", data=form_data)
+    data = response.json()
+    if response.status_code == 200:
+        assert data["email"] == email
+        assert data["message"] == "logged in"
+    return response.cookies.get("session_id")
+
+
+def profile_unlogged() -> None:
+    """ profile unlogged test method"""
+    response = requests.get("http://localhost:5000/profile")
+    assert response.status_code == 403
+
+
+def profile_logged(session_id: str) -> None:
+    """ Profile logged test method"""
+    cookies = {
+        "session_id": session_id
+    }
+    response = requests.get("http://localhost:5000/profile", cookies=cookies)
+    assert response.status_code == 200
+
+
+def log_out(session_id: str) -> None:
+    """ testing log_out"""
+    cookies = {
+        "session_id": session_id
+    }
+    response = requests.delete("http://localhost:5000/sessions",
+                               cookies=cookies, allow_redirects=False)
+    # print(response.status_code)
+    assert response.status_code in {301, 302, 303, 307, 308}
+    assert 'Location' in response.headers
+
+    assert response.headers['Location'] == "/"
+
+
+def reset_password_token(email: str) -> str:
+    """ reset password token test"""
+    form_data = {
+        "email": email
+    }
+    response = requests.post("http://localhost:5000/reset_password")
+    if response.status_code == 200:
+        data = response.json()
+        assert data["email"] == email
+        return data["reset_token"]
+    return None
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """ update password testing"""
+    form_data = {
+        "email": email,
+        "reset_token": reset_token,
+        "new_password": new_password
+    }
+    response = requests.put("http://localhost:5000/reset_password")
+    if response.status_code == 200:
+        data = response.json()
+        assert data["email"] == email
+        assert data["message"] == "Password updated"
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
